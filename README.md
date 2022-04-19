ea0fc2ad0ed4705520550d83c3eef96a360cbdc1
jeremy aubrey <jeremyaubrey@gmail.com>
2022-04-18 19:46:01 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Data Analysis
+
+A java program that reads historical gas data from a file and performs statistical analysis.
+
---
+++ src/Program8.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+//********************************************************************
+//
+//  Author:        Jeremy Aubrey
+//
+//  Program #:     8
+//
+//  File Name:     Program8.java
+//
+//  Course:        COSC 4301 - Modern Programming
+//
+//  Due Date:      04/20/2022
+//
+//  Instructor:    Fred Kumi 
+//
+//  Description:   TODO 
+//
+//********************************************************************
+
+public class Program8 {
+
+	public static void main(String[] args) {
+		
+		Program8 test = new Program8();
+		test.developerInfo();
+		
+	}
+	
+    //***************************************************************
+    //
+    //  Method:       developerInfo (Non Static)
+    // 
+    //  Description:  The developer information method of the program.
+    //
+    //  Parameters:   None
+    //
+    //  Returns:      N/A 
+    //
+    //**************************************************************
+    public void developerInfo()
+    {
+       System.out.println("Name:    Jeremy Aubrey");
+       System.out.println("Course:  COSC 4301 Modern Programming");
+       System.out.println("Program: 8\n");
+
+    } // end developerInfo method
+}
